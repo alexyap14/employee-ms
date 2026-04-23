77be88560fa0b1c251d13036b20d9e023c2442c5
Tatzuki <blueryo566@gmail.com>
2026-04-24 04:13:58 +0800

    Add Global Exception Handler for Task 1

---
+++ emloyeems/src/main/java/lk/linex/emloyeems/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package lk.linex.emloyeems.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, String>> handleException(Exception ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", ex.getMessage());
+
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
